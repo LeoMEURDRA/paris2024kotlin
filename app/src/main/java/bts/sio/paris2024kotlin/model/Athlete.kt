553a977a7e75074a72586ebc82f9804da5b2e5ab
LeoMEURDRA <leo.meurdra23@gmail.com>
2025-03-10 16:07:24 +0100

    Modification de la classes athletes pour s'adapter aux données envoyé par l'api (dateNaiss -> String au lieu de LocalDate) et de la liste des athletes pour utiliser l'api/la bdd

--- app/src/main/java/bts/sio/paris2024kotlin/model/Athlete.kt
+++ app/src/main/java/bts/sio/paris2024kotlin/model/Athlete.kt
@@ -1,10 +1,8 @@
 package bts.sio.paris2024kotlin.model
 
-import java.time.LocalDate
-
 data class Athlete(
     val id: Int,
     val nom: String,
     val prenom: String,
-    val dateNaiss: LocalDate,
+    val dateNaiss: String,
 )
--- app/src/main/java/bts/sio/paris2024kotlin/views/athlete/AthleteList.kt
+++ app/src/main/java/bts/sio/paris2024kotlin/views/athlete/AthleteList.kt
@@ -1,24 +1,28 @@
 package bts.sio.paris2024kotlin.views.athlete
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 import bts.sio.paris2024kotlin.viewmodel.athlete.AthleteViewModel
+import androidx.compose.material3.Text
 
 @Composable
 fun AthleteList(viewModel: AthleteViewModel = viewModel()) {
     val athletes by viewModel.athletes.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
+    val errorMessage by viewModel.errorMessage.collectAsState()
 
-    LazyColumn(
-        modifier = Modifier.padding(8.dp)
-    ) {
-        items(athletes) { athlete ->
-            AthleteCard(athlete = athlete)
+    when {
+        isLoading -> CircularProgressIndicator()
+        errorMessage != null -> Text(text = errorMessage!!, color = Color.Red)
+        else -> LazyColumn {
+            items(athletes) { athlete ->
+                AthleteCard(athlete = athlete)
+            }
         }
     }
 }
