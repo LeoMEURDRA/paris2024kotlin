a5bae32b58f90bddf4a6c7096ab843bc227bb8ab
LeoMEURDRA <leo.meurdra23@gmail.com>
2025-03-10 16:13:12 +0100

    Modification de la liste des olympiades pour utiliser l'api/la bdd

--- app/src/main/java/bts/sio/paris2024kotlin/viewmodel/olympiade/OlympiadeViewModel.kt
+++ app/src/main/java/bts/sio/paris2024kotlin/viewmodel/olympiade/OlympiadeViewModel.kt
@@ -1,8 +1,9 @@
 package bts.sio.paris2024kotlin.viewmodel.olympiade
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import bts.sio.paris2024kotlin.model.Olympiade
+import androidx.lifecycle.viewModelScope
+import bts.sio.paris2024kotlin.api.RetrofitInstance
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -12,17 +13,33 @@ class OlympiadeViewModel : ViewModel() {
     private val _olympiades = MutableStateFlow<List<Olympiade>>(emptyList())
     val olympiades: StateFlow<List<Olympiade>> = _olympiades
 
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage: StateFlow<String?> = _errorMessage
+
     init {
-        getAthletes()
+        getOlympiades()
     }
 
-    private fun getAthletes() {
+
+    private fun getOlympiades() {
         viewModelScope.launch {
-            _olympiades.value = listOf(
-                Olympiade(1, "XXXIII", 2024, "Paris"),
-                Olympiade(2, "XXXII", 2020, "Tokyo"),
-                Olympiade(3, "XXXI", 2016, "Rio"),
-            )
+            _isLoading.value = true
+            _errorMessage.value = null
+
+            try {
+                val response = RetrofitInstance.api.getOlympiades()
+                _olympiades.value = response
+            } catch (e: Exception) {
+                _errorMessage.value = "Erreur : ${e.localizedMessage ?: "Une erreur s'est produite"}"
+            } finally {
+                _isLoading.value = false
+                println("Chargement terminé")
+            }
         }
     }
+
+
 }
