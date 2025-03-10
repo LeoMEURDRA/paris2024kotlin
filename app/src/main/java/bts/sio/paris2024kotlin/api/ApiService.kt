d546633b94d072626322a5cc15d8b8a857bd31d9
LeoMEURDRA <leo.meurdra23@gmail.com>
2025-03-10 16:25:02 +0100

    Modification de la liste des pays pour utiliser l'api/la bdd

--- app/src/main/java/bts/sio/paris2024kotlin/api/ApiService.kt
+++ app/src/main/java/bts/sio/paris2024kotlin/api/ApiService.kt
@@ -2,6 +2,7 @@ package bts.sio.paris2024kotlin.api
 
 import bts.sio.paris2024kotlin.model.Athlete
 import bts.sio.paris2024kotlin.model.Olympiade
+import bts.sio.paris2024kotlin.model.Pays
 import bts.sio.paris2024kotlin.model.Sport
 import bts.sio.paris2024kotlin.model.Site
 import retrofit2.http.GET
@@ -18,4 +19,7 @@ interface ApiService {
 
     @GET("/sites")
     suspend fun getSites(): List<Site>
+
+    @GET("/pays")
+    suspend fun getPays(): List<Pays>
 }
