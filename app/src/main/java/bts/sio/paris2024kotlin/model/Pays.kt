6105dcc427aaeab892443fabf35fad2bd32904fb
LeoMEURDRA <leo.meurdra23@gmail.com>
2025-03-10 14:12:51 +0100

    Création de la liste des Pays, ou l'on retrouve le nom du pays

---
+++ app/src/main/java/bts/sio/paris2024kotlin/model/Pays.kt
@@ -0,0 +1,6 @@
+package bts.sio.paris2024kotlin.model
+
+data class Pays(
+    val id: Int,
+    val nom: String
+)
--- app/src/main/java/bts/sio/paris2024kotlin/views/AppNavigation.kt
+++ app/src/main/java/bts/sio/paris2024kotlin/views/AppNavigation.kt
@@ -1,6 +1,5 @@
 package bts.sio.paris2024kotlin.views
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -8,6 +7,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import bts.sio.paris2024kotlin.views.athlete.AthleteList
 import bts.sio.paris2024kotlin.views.olympiade.OlympiadeList
+import bts.sio.paris2024kotlin.views.pays.PaysList
 import bts.sio.paris2024kotlin.views.site.SiteList
 import bts.sio.paris2024kotlin.views.sport.SportList
 
@@ -28,7 +28,7 @@ fun AppNavigation(navController: NavHostController, modifier: Modifier = Modifie
             OlympiadeList()
         }
         composable("pays_list") {
-            Text("Page pays")
+            PaysList()
         }
         composable("sites_list") {
             SiteList()
